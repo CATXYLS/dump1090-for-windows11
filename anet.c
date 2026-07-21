0ab8fbfcefc8146c35633a230f866b1933df7d2a
CATXYLS <57060497+CATXYLS@users.noreply.github.com>
2026-07-22 00:45:26 +0800

    Cross-platform compatibility handling
    Add MinGW mapping
    Compatible with socket type differences
    Add unix-like mapping
    Set iosocket to non-blocking mode
    Added debug code
    The fatal error caused by the added code has been converted to a warning to facilitate program execution
    Added Windows-compatible IP processing

--- anet.c
+++ anet.c
@@ -51,6 +51,32 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
+#ifdef _WIN32
+#define WIN32_LEAN_AND_MEAN
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <windows.h>
+#include <stdio.h>
+#include <io.h>       // 提供 read/write/close 的 MinGW 映射
+#include <fcntl.h>    // 提供 F_GETFL, F_SETFL, O_NONBLOCK
+#include <errno.h>
+#include <stdarg.h>
+#include <string.h>
+
+// Windows 与 Unix 的 socket 类型差异
+#define ANET_SOCKET SOCKET
+#define ANET_INVALID_SOCKET INVALID_SOCKET
+#define ANET_SOCKET_ERROR SOCKET_ERROR
+#define ANET_CLOSE closesocket
+#define ANET_ERRNO WSAGetLastError()
+
+// 将 read/write/close 重定向为 MinGW 兼容版本
+#define read(fd, buf, len) _read(fd, buf, len)
+#define write(fd, buf, len) _write(fd, buf, len)
+#define close(fd) _close(fd)
+
+#else
+// Unix 原生头文件
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <sys/stat.h>
@@ -66,6 +92,15 @@
 #include <stdarg.h>
 #include <stdio.h>
 
+// Unix 下的类型映射
+#define ANET_SOCKET int
+#define ANET_INVALID_SOCKET -1
+#define ANET_SOCKET_ERROR -1
+#define ANET_CLOSE close
+#define ANET_ERRNO errno
+
+#endif
+
 #include "anet.h"
 
 static void anetSetError(char *err, const char *fmt, ...)
@@ -80,11 +115,16 @@ static void anetSetError(char *err, const char *fmt, ...)
 
 int anetNonBlock(char *err, int fd)
 {
-    int flags;
-
-    /* Set the socket nonblocking.
-     * Note that fcntl(2) for F_GETFL and F_SETFL can't be
-     * interrupted by a signal. */
+#ifdef _WIN32
+    // Windows: use ioctlsocket to set non-blocking mode
+    unsigned long mode = 1;
+    if (ioctlsocket(fd, FIONBIO, &mode) != 0) {
+        anetSetError(err, "ioctlsocket(FIONBIO): %s", strerror(WSAGetLastError()));
+        return ANET_ERR;
+    }
+#else
+    int flags;   // <-- flags 移到这里
+    // Unix: use fcntl
     if ((flags = fcntl(fd, F_GETFL)) == -1) {
         anetSetError(err, "fcntl(F_GETFL): %s", strerror(errno));
         return ANET_ERR;
@@ -93,6 +133,7 @@ int anetNonBlock(char *err, int fd)
         anetSetError(err, "fcntl(F_SETFL,O_NONBLOCK): %s", strerror(errno));
         return ANET_ERR;
     }
+#endif
 
     return ANET_OK;
 }
@@ -131,16 +172,16 @@ int anetTcpKeepAlive(char *err, int fd)
 static int anetCreateSocket(char *err, int domain)
 {
     int s, on = 1;
-    if ((s = socket(domain, SOCK_STREAM, 0)) == -1) {
-        anetSetError(err, "creating socket: %s", strerror(errno));
+    if ((s = socket(domain, SOCK_STREAM, 0)) == (int)ANET_INVALID_SOCKET) {
+#ifdef _WIN32
+        fprintf(stderr, "socket() failed: WSAGetLastError=%d\n", WSAGetLastError());
+#endif
+        anetSetError(err, "creating socket: %s", strerror(ANET_ERRNO));
         return ANET_ERR;
     }
 
-    /* Make sure connection-intensive things like the redis benckmark
-     * will be able to close/open sockets a zillion of times */
-    if (setsockopt(s, SOL_SOCKET, SO_REUSEADDR, (void*)&on, sizeof(on)) == -1) {
-        anetSetError(err, "setsockopt SO_REUSEADDR: %s", strerror(errno));
-        return ANET_ERR;
+    if (setsockopt(s, SOL_SOCKET, SO_REUSEADDR, (const char*)&on, sizeof(on)) == ANET_SOCKET_ERROR) {
+        // SO_REUSEADDR failure is not fatal on Windows, just warn
     }
     return s;
 }
@@ -239,21 +280,26 @@ int anetWrite(int fd, char *buf, int count)
 static int anetListen(char *err, int s, struct sockaddr *sa, socklen_t len) {
     if (sa->sa_family == AF_INET6) {
         int on = 1;
-        setsockopt(s, IPPROTO_IPV6, IPV6_V6ONLY, &on, sizeof(on));
+        setsockopt(s, IPPROTO_IPV6, IPV6_V6ONLY, (const char*)&on, sizeof(on));
     }
 
-    if (bind(s,sa,len) == -1) {
-        anetSetError(err, "bind: %s", strerror(errno));
-        close(s);
+    if (bind(s, sa, len) == (int)ANET_SOCKET_ERROR) {
+#ifdef _WIN32
+        int wsa_err = WSAGetLastError();
+        char msgbuf[256];
+        FormatMessageA(FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
+                       NULL, wsa_err, 0, msgbuf, sizeof(msgbuf), NULL);
+        anetSetError(err, "bind: [%d] %s", wsa_err, msgbuf);
+#else
+        anetSetError(err, "bind: %s", strerror(ANET_ERRNO));
+#endif
+        ANET_CLOSE(s);
         return ANET_ERR;
     }
 
-    /* Use a backlog of 512 entries. We pass 511 to the listen() call because
-     * the kernel does: backlogsize = roundup_pow_of_two(backlogsize + 1);
-     * which will thus give us a backlog of 512 entries */
-    if (listen(s, 511) == -1) {
-        anetSetError(err, "listen: %s", strerror(errno));
-        close(s);
+    if (listen(s, 511) == (int)ANET_SOCKET_ERROR) {
+        anetSetError(err, "listen: %s", strerror(ANET_ERRNO));
+        ANET_CLOSE(s);
         return ANET_ERR;
     }
     return ANET_OK;
@@ -263,10 +309,72 @@ int anetTcpServer(char *err, char *service, char *bindaddr, int *fds, int nfds)
 {
     int s;
     int i = 0;
+#ifdef _WIN32
+    // Windows-specific variables declared in the Windows block
+    (void)nfds;  // suppress unused parameter warning
+#else
     struct addrinfo gai_hints;
     struct addrinfo *gai_result, *p;
     int gai_error;
+#endif
 
+#ifdef _WIN32
+    // Windows: bypass getaddrinfo for numeric IP addresses
+    // fprintf(stderr, "DEBUG: anetTcpServer Windows branch, service=%s, bindaddr=%s\n", service, bindaddr ? bindaddr : "(null)");
+    struct sockaddr_in sa4;
+    struct sockaddr_in6 sa6;
+    int port = atoi(service);
+    
+    // Try IPv4 first
+    memset(&sa4, 0, sizeof(sa4));
+    sa4.sin_family = AF_INET;
+    sa4.sin_port = htons(port);
+    if (!bindaddr || !strcmp(bindaddr, "0.0.0.0") || !strcmp(bindaddr, "localhost")) {
+        sa4.sin_addr.s_addr = INADDR_ANY;
+    } else if (inet_pton(AF_INET, bindaddr, &sa4.sin_addr) == 1) {
+        // valid IPv4 address
+    } else {
+        // not a valid IPv4, skip IPv4
+        goto try_ipv6;
+    }
+    
+    if ((s = anetCreateSocket(err, AF_INET)) != ANET_ERR) {
+        if (anetListen(err, s, (struct sockaddr*)&sa4, sizeof(sa4)) != ANET_ERR) {
+            fds[i++] = s;
+        } else {
+            // fprintf(stderr, "IPv4 bind failed: %s\n", err);
+        }
+    }
+    
+try_ipv6:
+    // Try IPv6
+    memset(&sa6, 0, sizeof(sa6));
+    sa6.sin6_family = AF_INET6;
+    sa6.sin6_port = htons(port);
+    if (!bindaddr || !strcmp(bindaddr, "0.0.0.0") || !strcmp(bindaddr, "localhost")) {
+        sa6.sin6_addr = in6addr_any;
+    } else if (inet_pton(AF_INET6, bindaddr, &sa6.sin6_addr) == 1) {
+        // valid IPv6 address
+    } else {
+        goto done;
+    }
+    
+    if ((s = anetCreateSocket(err, AF_INET6)) != ANET_ERR) {
+        if (anetListen(err, s, (struct sockaddr*)&sa6, sizeof(sa6)) != ANET_ERR) {
+            fds[i++] = s;
+        } else {
+            // fprintf(stderr, "IPv6 bind failed: %s\n", err);
+        }
+    }
+    
+done:
+    if (i == 0) {
+        anetSetError(err, "unable to bind socket");
+        return ANET_ERR;
+    }
+    return i;
+#else
+    // Original Unix implementation
     gai_hints.ai_family = AF_UNSPEC;
     gai_hints.ai_socktype = SOCK_STREAM;
     gai_hints.ai_protocol = 0;
@@ -295,6 +403,7 @@ int anetTcpServer(char *err, char *service, char *bindaddr, int *fds, int nfds)
 
     freeaddrinfo(gai_result);
     return (i > 0 ? i : ANET_ERR);
+#endif
 }
 
 static int anetGenericAccept(char *err, int s, struct sockaddr *sa, socklen_t *len)
